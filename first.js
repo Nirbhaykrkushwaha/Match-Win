3360db5bebd9388dc94e40373736815b8260de5a
Nirbhaykrkushwaha <kushwahanirbhaykumar7@gmail.com>
2024-02-25 21:48:07 +0530

    first changes

---
+++ first.js
@@ -0,0 +1,23 @@
+let modebtn=document.querySelector("#mode")
+let currMode="light";
+let body=document.querySelector("body")
+
+modebtn.addEventListener("click",()=>
+{
+  if(currMode==="light")
+  {
+    currMode="dark";
+    body.classList.add("dark")
+    body.classList.remove("light")
+
+  }
+  else
+  {
+    currMode="light";
+    body.classList.add("light")
+    body.classList.remove("dark")
+
+  }
+  console.log(currMode);
+}
+)
